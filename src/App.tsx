c6edf3d97898cc32db6b226ba886d414200af967
LEO GOUBARD <goubardleo@gmail.com>
2024-01-18 17:04:37 +0100

    base: the base of the app, using tailwind, typescript, eslint plugins, postcss, prettier

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+export default function App() {
+	return (
+		<div className="flex h-screen items-center justify-center">
+			<div className="text-center">
+				<h1 className="mb-4 text-4xl font-bold">Hello World!</h1>
+				<p className="mb-4 text-lg">
+					This is a starter template for React projects with Tailwind CSS and
+					TypeScript.
+				</p>
+			</div>
+		</div>
+	)
+}
