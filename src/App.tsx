954165bfad5a4a9abb2f82e84141ea4bccda1e81
LEO GOUBARD <goubardleo@gmail.com>
2024-01-20 23:29:31 +0100

    feat: Just add the classes with the merges ones of the basic Buttons, setup 3 cases

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,46 @@
 import Section from "./components/site/Section";
 import Container from "./components/site/Container";
 import Layout from "./components/site/Layout";
+import Button from "./components/library/Button";
 
 export default function App() {
 	return (
     <Layout>
       <Container
-        title="Container heading using a component instead of hardcoding"
-        description="Container description"
+        title="Button"
+        description="A button is a widget that enables users to trigger an action or event"
       >
 
 
         {/* Section */}
         <Section
-          title="Section heading using a component instead of hardcoding"
-          description="Section description"
+          title="Primary"
+          description="Primary actions should be obvious. Solid, high contast background colors work great here."
         >
 
-          <div className="bg-blue-500/50 p-3">component 1</div>
-          <div className="bg-blue-500/50 p-3">component 2</div>
+          <Button>Default Primary</Button>
         </Section>
 
         <Section
-          title="Section heading using a component instead of hardcoding"
-          description="Section description"
+          title="Secondary"
+          description="Secondary actions should be obvious. Solid, high contast background colors work great here."
+        >
+
+          <Button variant="secondary">Default Secondary</Button>
+
+        </Section>
+
+        <Section
+          title="Tertiary"
+          description="Tertiary actions should be obvious. Solid, high contast background colors work great here."
+        >
+
+          <Button variant="tertiary">Default Tertiary</Button>
+        </Section>
+
+        <Section
+          title="Destructive"
+          description="Destructive actions should be obvious. Solid, high contast background colors work great here."
         >
 
           <div className="bg-blue-500/50 p-3">component 1</div>
